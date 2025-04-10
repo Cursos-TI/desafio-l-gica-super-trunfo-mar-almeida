fd7417c7529e55b27535222f578a10cce23b9e10
Marco Almeida <mar.almeida@outlook.com>
2025-04-10 21:16:51 +0000

    Desafio Logica Super Trunfo Novato Completo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
 
     char nome1[50]; // Paraná
@@ -201,5 +196,5 @@ int main() {
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
 
     return 0;
-    
+
 }
